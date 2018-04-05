7fc0e76eeceb6af0bc7227018a8c6119b2ec492c
Karel_Sir <karel.sir@tul.cz>
2018-04-05 07:53:59 +0200

    shell fix

--- shell.sh
+++ shell.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "4" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio4/direction
+echo "23" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio23/direction
 
 while true
 do
-	echo 1 > /sys/class/gpio/gpio4/value
-	echo 0 > /sys/class/gpio/gpio4/value
+	echo 1 > /sys/class/gpio/gpio23/value
+	echo 0 > /sys/class/gpio/gpio23/value
 done
