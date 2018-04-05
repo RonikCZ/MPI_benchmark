46b9c894fd9198d7e7311f26ba00b64621c47951
Karel_Sir <karel.sir@tul.cz>
2018-04-05 07:52:30 +0200

    python fix

--- python.py
+++ python.py
@@ -1,9 +1,11 @@
 import RPi.GPIO as GPIO
 
+GPIO.setwarnings(False)
+
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(4, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
 
 while True:
-    GPIO.output(4, True)
-    GPIO.output(4, False)
+    GPIO.output(23, True)
+    GPIO.output(23, False)
